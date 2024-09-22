6220a9d32d9942fc06c52126deac7d6c01cc64a5
hbery <a.twardosz98@gmail.com>
2024-09-22 14:14:42 +0200

    chore: move scripts to common/
    chore: rename Dockerfiles to Containerfiles

--- common/scripts/build_containers.sh
+++ common/scripts/build_containers.sh
@@ -7,16 +7,20 @@ _project_root="$(git rev-parse --show-toplevel)"
 pushd "${_project_root}" &>/dev/null || exit 1
 
 # build new containers
-for _container in $(find "${_project_root}" -type d -name "*box"); do
+while read -r _container; do
     _container_name="$(basename "${_container}")"
 
     pushd "${_container}" &>/dev/null || exit 1
     echo "*** Building ${_container_name}.."
-    docker build -t "${_container_name}:latest" .
+    docker build -t "${_container_name}:latest" -f Containerfile .
     popd &>/dev/null || exit 1
-done
+done < <(find "${_project_root}" -type d -name "*box")
 
 # remove dangling
-docker image rm $(docker image ls --filter "dangling=true" --quiet)
+declare -a _dangling_containers
+mapfile -t _dangling_containers < <(docker image ls --filter "dangling=true" --quiet)
+if [[ "${_dangling_containers[*]}" != "" ]]; then
+    docker image rm "${_dangling_containers[@]}"
+fi
 
 popd &>/dev/null || exit 1
